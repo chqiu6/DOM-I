8457515dc93747059b24f5517ff1f84fea651096
chqiu6 <chucky-qiu@lambdastudents.com>
2020-06-05 16:50:56 -0400

    finished except stretch

--- js/index.js
+++ js/index.js
@@ -40,3 +40,54 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//header
+const navEdit = document.querySelectorAll("a");
+navEdit[0].textContent = "Services";
+navEdit[1].textContent = "Products";
+navEdit[2].textContent = "Visions";
+navEdit[3].textContent = "Features";
+navEdit[4].textContent = "About";
+navEdit[5].textContent = "Contact";
+navEdit[0].style.color = "red";
+
+//cta
+const ctaEdith1 = document.getElementsByTagName('h1');
+ctaEdith1[0].textContent = "DOM  Is Awesome";
+
+
+const buttonEdit = document.getElementsByTagName("button");
+buttonEdit[0].textContent = "Get Started";
+
+
+const imgEdit = document.querySelector('#cta-img');
+imgEdit.src = "img/header-img.png";
+
+//main-content
+const featureshEdit = document.querySelectorAll("h4");
+featureshEdit[0].textContent = "Features";
+featureshEdit[1].textContent = "About";
+featureshEdit[2].textContent = "Services";
+featureshEdit[3].textContent = "Product";
+featureshEdit[4].textContent = "Vision";
+featureshEdit[5].textContent = "Contact";
+
+const midimgEdit = document.querySelector(".middle-img");
+midimgEdit.src = "img/mid-page-accent.jpg";
+
+const featurespEdit = document.querySelectorAll("p");
+featurespEdit[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+featurespEdit[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+featurespEdit[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+featurespEdit[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+featurespEdit[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+//contact
+featurespEdit[5].textContent = "123 Way 456 Street Somewhere, USA";
+featurespEdit[6].textContent = "1 (888) 888-8888";
+featurespEdit[7].textContent = "sales@greatidea.io"
+
+//footer
+featurespEdit[8].textContent = "Copyright Great Idea! 2018";
+
+
